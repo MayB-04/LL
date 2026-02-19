34d5119fe355ff7eab669d6981c0d18efeb1ba3e
MayB-04 <maybadenhorst@gmail.com>
2026-02-19 14:20:02 +0200

    landing page: making landing page

--- script.js
+++ script.js
@@ -2,3 +2,28 @@ function startGame() {
     alert("The game is starting!");
     console.log("Button clicked!");
 }
+const kissBtn = document.getElementById('surprise-kiss');
+function surpriseKiss(){
+    document.body.classList.add('pink-background');
+
+    const kissContainer = document.createElement('div');
+    kissContainer.classList.add('kiss-overlay');
+
+    const mwah = document.createElement('span');
+    mwah.innerText = "Mwah!";
+    mwah.classList.add('mwah-text');
+
+    const emoji = document.createElement('span');
+    emoji.innerText = "💋";
+    emoji.style.fontSize = "100px"; 
+
+    kissContainer.appendChild(mwah);
+    kissContainer.appendChild(emoji);
+    document.body.appendChild(kissContainer);
+
+    setTimeout(() => {
+        kissContainer.remove();
+    document.body.classList.remove('pink-background');
+   },2000);
+}
+kissBtn.addEventListener('click',surpriseKiss);
