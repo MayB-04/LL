d1a77e2d59828ec7cd18e75da0a050570db867fd
MayB-04 <maybadenhorst@gmail.com>
2026-02-20 11:35:28 +0200

    landing page feature, making first mini game

--- page1.js
+++ page1.js
@@ -3,3 +3,39 @@ function landingPage(){
     window.location.href = "index.html";
 }
 backBtnLanding.addEventListener('click',landingPage);
+
+const bug = document.getElementById('bug');
+const container = document.getElementById('game-container');
+
+let posX = -100;      
+let posY = 100;       
+let targetY = 100;    
+let speedX = 2;       
+let speedY = 0;       
+
+function flutterAcross() {
+    const width = container.clientWidth;
+    posX += speedX;
+
+    if (Math.random() > 0.97) { 
+        targetY = 50 + Math.random() * 150; 
+    }
+
+    let distanceToTarget = targetY - posY;
+    speedY += distanceToTarget * 0.01; 
+    speedY *= 0.85;                    
+    posY += speedY;
+
+    
+    bug.style.left = posX + "px";
+    bug.style.top = posY + "px";
+    bug.style.transform = `rotate(${speedY * 8}deg)`;
+
+    if (posX > width + 100) {
+        posX = -100;
+        posY = 100; 
+    }
+
+    requestAnimationFrame(flutterAcross);
+}
+flutterAcross();
